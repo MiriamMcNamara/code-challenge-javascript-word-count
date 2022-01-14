fabc5792d19d65ab4bc70fd60fb33bb33eedf5e4
Miriam McNamara <miriammcnamara@icloud.com>
2022-01-14 11:11:21 -0600

    working with my own test strings

--- word-count.js
+++ word-count.js
@@ -7,20 +7,34 @@
 const countWords = (str) => {
   //declare a regex variable
   let regexp = /[\w']+/gim;
+  //declare an object to return (pretty sure that's what the return wants based on test returns)
+  let wordCount = {};
+  console.log(wordCount);
 
   //declare a variable for the regex situation
-  let matchAll = str.matchAll(regexp);
-
-  //show what returns, a big nasty object we need to turn into an array
-  console.log(matchAll);
-  //make it into an array
-  matchAll = Array.from(matchAll);
+  let matchAll = str.match(regexp);
 
   //loop through the array and show all the matches
   for (match in matchAll) {
-    console.log(matchAll[match]);
+    console.log("match:", matchAll[match]);
     //okay this we can work with!
+    //declare variable for my brain
+    let word = matchAll[match];
+    //how to compare it to wordCount? loop within loop too complicated
+    //if it doesn't exist it returns undefined
+    console.log(wordCount[word]);
+    //if there is no wordCount[word], declare it as a property with value of 1
+    if (wordCount[word] === undefined) {
+      wordCount[word] = 1;
+      //otherwise, increment
+    } else {
+      wordCount[word]++;
+    }
+
+    console.log(wordCount);
   }
 };
 
-countWords("hello, world!");
+countWords(
+  "1,000 rainbows are better and are more prettier than 999 rainbows..."
+);
