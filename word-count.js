40e2fd8be0bdd18899a32c0eee87ed5f228eebaf
Miriam McNamara <miriammcnamara@icloud.com>
2022-01-14 09:10:54 -0600

    initial setup and testing

---
+++ word-count.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// export (add this back in when submitting)
+const countWords = () => {
+  console.log("in countWords");
+  //testing testing; having some issues setting up git
+};
+
+countWords();
