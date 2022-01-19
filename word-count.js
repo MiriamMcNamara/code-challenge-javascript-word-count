d611d80c19d1ea2f66036b733b0087c7df8f38d4
Miriam McNamara <miriammcnamara@icloud.com>
2022-01-19 12:00:46 -0600

    all tests passing

--- word-count.js
+++ word-count.js
@@ -1,40 +1,31 @@
-//
-// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// export (add this back in when testing/submitting)
-const countWords = (str) => {
-  //declare a regex variable
+export const countWords = (str) => {
+  //declare a regex variable to capture words from the string
   let regexp = /[\w']+/gim;
-  //declare an object to return (pretty sure that's what the return wants based on test returns)
+  //declare an object to return
   let wordCount = {};
-  console.log(wordCount);
 
   //declare a variable for the regex situation
   let matchAll = str.match(regexp);
 
   //loop through the array and show all the matches
-  for (match in matchAll) {
-    console.log("match:", matchAll[match]);
-    //okay this we can work with!
-    //declare variable for my brain
-    let word = matchAll[match];
-    //how to compare it to wordCount? loop within loop too complicated
-    //if it doesn't exist it returns undefined
-    console.log(wordCount[word]);
-    //if there is no wordCount[word], declare it as a property with value of 1
+  for (let i = 0; i < matchAll.length; i++) {
+    //declare variable for each word, control case
+    let word = matchAll[i].toLowerCase();
+    console.log("word:", word);
+    //remove any unwanted single quotes included by the regex
+    if (word.charAt(0) === "'") {
+      word = word.replace(word.charAt(0), "");
+    }
+    if (word.charAt(word.length - 1) === "'") {
+      word = word.replace(word.charAt(word.length - 1), "");
+    }
+    //if there is no wordCount[word] (undefined), declare it as a property with value of 1
     if (wordCount[word] === undefined) {
       wordCount[word] = 1;
       //otherwise, increment
     } else {
       wordCount[word]++;
     }
-
-    console.log(wordCount);
   }
+  return wordCount;
 };
-
-countWords(
-  "1,000 rainbows are better and are more prettier than 999 rainbows..."
-);
